f2b4cb3a0b50faef0631721ea3685d83411c3b1e
Lukw07 <kry.tuma@gmail.com>
2024-12-02 12:18:31 +0100

    Footer, header, partneři

--- Monoska/src/Components/Footer.jsx
+++ Monoska/src/Components/Footer.jsx
@@ -1,69 +1,81 @@
 import React from "react";
-import '../styles/Footer.css'; 
+import "../styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarker,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  // Získání aktuálního roku
-  const currentYear = new Date().getFullYear();
-
+    const rok = new Date().getFullYear();
   return (
-    <footer id="footer">
-      <div className="col col1">
-        <h3>Monosh</h3>
-        <p>
-          Vyrobil <span style={{ color: "#BA6573" }}>❤</span> Kryštof Tůma
-        </p>
-        <div className="social">
-          <a
-            href="https://codepen.io/Juxtopposed"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            <img
-              src="https://assets.codepen.io/9051928/codepen_1.png"
-              alt="CodePen"
-            />
-          </a>
-          <a
-            href="https://twitter.com/juxtopposed"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            <img
-              src="https://assets.codepen.io/9051928/x.png"
-              alt="Twitter"
-            />
-          </a>
-          <a
-            href="https://youtube.com/@juxtopposed"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            <img
-              src="https://assets.codepen.io/9051928/youtube_1.png"
-              alt="YouTube"
-            />
-          </a>
+    <footer className="footer-distributed">
+      <div className="footer-left">
+        <img src="./src/assets/Snímek obrazovky 2024-12-02 085635.png" alt="Logo" width="450" height="300" />
+      </div>
+
+      <div className="footer-center">
+        <div>
+          <iframe src="https://maps.google.com/maps?width=450&amp;height=300&amp;hl=en&amp;q=monosh%20s.r.o.+(Armax)&amp;ie=UTF8&amp;t=&amp;z=12&amp;iwloc=B&amp;output=embed" width="450" height="300" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" data-ruffle-polyfilled=""></iframe>
+          <FontAwesomeIcon icon={faMapMarker} />
+          <p>
+            <span>MONOSH s.r.o.
+            Podmostní 39/5</span>405 02 Děčín 7, CZ
+          </p>
         </div>
-        <p style={{ color: "#818181", fontSize: "smaller" }}>
-          {currentYear} © All Rights Reserved
+
+        <div>
+          <FontAwesomeIcon icon={faPhone} />
+          <p>Telefon 603 548 297</p>
+        </div>
+
+        <div>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <p>
+            <a href="mailto:info@monosh.cz">info@monosh.cz</a>
+          </p>
+        </div>
+      </div>
+
+      <div className="footer-right">
+        <p className="footer-company-about">
+          <span>O nás</span> Společnost MONOSH s.r.o. se specializuje na velkosériové, malosériové i kusové obrábění železných i neželezných kovů a plastů, navrhování a výrobu jednoúčelových strojů, linek a přípravků.
         </p>
+        
+        {/* Sekce pro partnery */}
+        <div className="footer-partners">
+          <h3>Naši partneři</h3>
+          <div className="partners-logos">
+            <img src="./src/assets/partner1.svg" alt="Partner 1" width="100" height="50" />
+            <img src="./src/assets/partner2.svg" alt="Partner 2" width="100" height="50" />
+            <img src="./src/assets/partner3.svg" alt="Partner 3" width="100" height="50" />
+            <img src="./src/assets/partner4.svg" alt="Partner 4" width="100" height="50" />
+            <img src="./src/assets/partner5.svg" alt="Partner 5" width="100" height="50" />
+            <img src="./src/assets/partner6.svg" alt="Partner 6" width="100" height="50" />
+            <img src="./src/assets/TRCZ-logo.jpg" alt="Partner 7" width="100" height="50" />
+          </div>
+        </div>
+        
+        <div className="footer-icons">
+          <h3>Soc. Sítě</h3>
+          <a href="https://www.facebook.com/profile.php?id=950484265015663&_rdr">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <p className="footer-company-name">Monosh &copy; {rok}</p>
+        </div>
       </div>
-      <div className="col col2">
-        <p>About</p>
-        <p>Our mission</p>
-        <p>Privacy Policy</p>
-        <p>Terms of service</p>
-      </div>
-      <div className="col col3">
-        <p>Services</p>
-        <p>Products</p>
-        <p>Join our team</p>
-        <p>Partner with us</p>
-      </div>
-      <div className="backdrop"></div>
     </footer>
   );
 };
--- Monoska/src/Components/Header.jsx
+++ Monoska/src/Components/Header.jsx
@@ -2,36 +2,21 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 import '../styles/Header.css';
 
-const languageOptions = [
-  { value: 'cz', label: 'CZ', flagUrl: 'https://www.iconarchive.com/download/i109116/wikipedia/flags/CZ-Czech-Republic-Flag.1024.png' },
-  { value: 'en', label: 'EN', flagUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg' },
-  { value: 'de', label: 'DE', flagUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg' },
-  { value: 'fr', label: 'FR', flagUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg' }
-];
-
 function Header() {
   const [language, setLanguage] = useState('cz');
 
   const handleLanguageChange = (selectedOption) => {
     setLanguage(selectedOption.value);
-    // Your logic for language change here
+    
   };
-
-  const customSingleValue = ({ data }) => (
-    <div className="custom-single-value">
-      <img src={data.flagUrl} alt={data.label} className="flag-icon" />
-      {data.label}
-    </div>
-  );
 
   return (
     <header className="header">
       <div className="logo">
-        <a href="localhost:5173">
+        <a href="http://localhost:5173">
           <img
             src="https://www.monosh.com/img/logo.png"
             alt="Logo"
-            srcSet=""
           />
         </a>
       </div>
@@ -45,18 +30,16 @@ function Header() {
           <li><a href="#kontakt">Kontakt</a></li>
         </ul>
       </nav>
-      <div className="language-selector">
+      <div>
         <Select
-          value={languageOptions.find(option => option.value === language)}
+          value={{ value: language, label: language.toUpperCase() }}
           onChange={handleLanguageChange}
-          options={languageOptions}
-          getOptionLabel={(e) => (
-            <div className="custom-option">
-              <img src={e.flagUrl} alt={e.label} className="flag-icon" />
-              {e.label}
-            </div>
-          )}
-          components={{ SingleValue: customSingleValue }}
+          options={[
+            { value: 'cz', label: 'CZ' },
+            { value: 'en', label: 'EN' },
+            { value: 'de', label: 'DE' },
+            { value: 'fr', label: 'FR' },
+          ]}
         />
       </div>
     </header>
